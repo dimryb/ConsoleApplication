79c5b550d2f4f5194a55ad852ffc77ca23ce754c
Dmitry <d.rybakov@cognitivepilot.com>
2022-05-11 09:46:45 +0700

    Тест rsa в отдельном файле

--- adapter.c
+++ adapter.c
@@ -2,10 +2,13 @@
 #include "adapter.h"
 #include "mbedtls/include/mbedtls/rsa.h"
 #include <stdio.h>
+#include "rsaTest.h"
 
 void adapter() {
 	int verbose = 1;
 	int result = mbedtls_rsa_self_test(verbose);
+
+	result = rsa_test(verbose);
 }
 
 int (*mbedtls_snprintf)(char* s, size_t n, const char* format, ...) = snprintf;
---
+++ rsaTest.h
@@ -0,0 +1,7 @@
+
+#ifndef RSA_TEST_H
+#define RSA_TEST_H
+
+int rsa_test(int verbose);
+
+#endif //RSA_TEST
